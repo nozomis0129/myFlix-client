eefa6fece67149ac86c91021d8b12cfbe94bf622
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-09-26 18:27:13 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,63 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      Title: "The Godfather",
-      Genre: {
-        Name: "Crime",
-        Description: "Crime film is a film genre inspired by and analogous to the crime fiction literary genre. Films of this genre generally involve various aspects of crime and its detection."
-      },
-      Description: "Michael, the young and idealistic son of Vito Corleone, the head of the most powerful Mafia clan in Ner York, returns home as a war hero and is determined to live his own life. But tragic circumstances make him face the legacy of his family.",
-      Director: {
-        Name: "Francis Ford Coppola",
-        Bio: "Francis Ford Coppola is an American film director, producer, and screenwriter. He is considered one of the major figures of the New Hollywood filmmaking movement of the 1960s and 1970s.",
-        Birth: 1939
-      },
-        ImagePath: "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
-        Release: 1972,
-        Featured:false
-    },
-    {
-      id: 2,
-      Title: "Eternal Sunshine Of The Spotless Mind","Description":"Joel Barish is shocked to discover that the love of his life, Clementine, has had erased from her memory. As a result, hurt and angry, Joel wants to pay her back in the same coin, going as far as to undergo a painless but intricate medical procedure to do the same. And as the once-cherished recollections of Clementine gradually fade away, giving away to a soulless black void, something happens. Now Joel has second thoughts, toying with the idea of stopping the irreversible process.",
-      Genre: {
-        Name: "Romance",
-        Description:"Romance films involve romantic love stories recorded in visual media for broadcast in theatres or on television that focus on passion, emotion, and the affectionate romantic involvement of the main characters."
-      },
-      Director: {
-        Name: "Michel Gondry",
-        Bio: "Michel Gondry is a French filmmaker noted for his inventive visual style. He won an Academy Award for Best Original Screenplay as one of the writers of the 2004 film Eternal Sunshine of the Spotless Mind, which he also directed.",
-        Birth: 1963
-      },
-      ImagePath: "https://upload.wikimedia.org/wikipedia/en/a/a4/Eternal_Sunshine_of_the_Spotless_Mind.png",
-      Release: 2004,
-      Featured: false
-    },
-    {
-      id: 3,
-      Title: "Pulp Fiction",
-      Description: "The lives of two hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-      Genre: {
-        Name: "Crime",
-        Description: "Crime film is a film genre inspired by and analogous to the crime fiction literary genre. Films of this genre generally involve various aspects of crime and its detection."
-      },
-      Director: {
-        Name: "Quentin Tarantino",
-        Bio: "Quentin Jerome Tarantino is an American film director, screenwriter, producer, actor, and author.",
-        Birth: 1963
-      },
-      ImagePath: "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
-      Release: 1994,
-      Featured: false
-    }
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  useEffect(() => {
+    fetch("https://movies-flix-app-bb16fed0a4c0.herokuapp.com/movies")
+      .then ((response) => response.json())
+      .then ((data) => {
+        console.log("movies from api:", data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id,
+            title: movie.Title,
+            description: movie.Description,
+            actors: movie.Actors,
+            genre: {
+              name: movie.Genre.Name,
+              description: movie.Genre.Description,
+            },
+            director: {
+              name: movie.Director.Name,
+              bio: movie.Director.Bio,
+              birth: movie.Director.Birth,
+              death: movie.Director.Death
+            },
+            image: movie.ImagePath,
+            release: movie.Release,
+            featured: movie.Featured
+          };
+        });
+        setMovies(moviesFromApi); 
+      });
+  }, []);
+
+  
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -65,7 +44,7 @@ export const MainView = () => {
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return <div>The Movie list is empty!</div>;
   }
 
   return (
